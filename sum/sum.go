47d5575b50491eedfe60147940b024a4d7e9fe3d
msfukui <msfukui@gmail.com>
2023-03-26 07:02:10 +0900

    SumAll(),SumAllTails()を実装してリファクタリングまで対応

--- sum/sum.go
+++ sum/sum.go
@@ -6,3 +6,24 @@ func Sum(members []int) (sum int) {
 	}
 	return sum
 }
+
+func SumAll(membersToSum ...[]int) (sums []int) {
+	for _, members := range membersToSum {
+		sums = append(sums, Sum(members))
+	}
+
+	return
+}
+
+func SumAllTails(membersToSum ...[]int) (sums []int) {
+	for _, members := range membersToSum {
+		if len(members) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := members[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,10 +1,13 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	t.Run("任意のサイズのスライスを指定する場合", func(t *testing.T) {
+	t.Run("整数が要素の任意のサイズのスライスを指定するとスライス内の数を合計して返す", func(t *testing.T) {
 		members := []int{1, 2, 3, 4, 5}
 		got := Sum(members)
 		want := 15
@@ -12,5 +15,42 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, members)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("整数が要素の任意のサイズのスライスを複数指定するとスライス毎に数を合計したスライスを返す", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(y *testing.T, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("整数が要素の任意のサイズのスライスを複数指定するとスライス毎の最初を除く合計の要素のスライスを返す", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("安全性のため空のスライスには0を返す", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
