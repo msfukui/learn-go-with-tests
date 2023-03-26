70064c7c1ebf6e67e94272b33a853b8b8a5c44f7
msfukui <msfukui@gmail.com>
2023-03-26 17:07:52 +0900

    Withdraw()を実装

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,25 @@
 package wallet
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
