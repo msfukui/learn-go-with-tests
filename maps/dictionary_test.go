e7f9c1c464dd72f4861d788d7486882936fcd5ee
msfukui <msfukui@gmail.com>
2023-04-02 11:26:40 +0900

    マップへの登録としてAdd()を実装

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -21,6 +21,21 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+
+	dictionary := Dictionary{}
+	dictionary.Add("テスト", "これがテストだ!")
+
+	got, err := dictionary.Search("テスト")
+	want := "これがテストだ!"
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
