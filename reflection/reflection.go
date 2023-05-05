dd2090e83a42caf3fae80f51f133ad3f76aff824
msfukui <msfukui@gmail.com>
2023-05-05 19:23:07 +0900

    walk() に入れ子構造の場合の再帰呼び出しの判定を追加

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -4,10 +4,17 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
+
 	for i := 0; i < val.NumField(); i++ {
+
 		field := val.Field(i)
+
 		if field.Kind() == reflect.String {
 			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
 		}
 	}
 }
