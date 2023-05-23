4489c9dd3c774915daf72f02321ea42d89f41cbe
msfukui <msfukui@gmail.com>
2023-05-23 18:36:51 +0900

    walk() に関数の実装を追加

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -25,9 +25,13 @@ func walk(x interface{}, fn func(input string)) {
 			walkValue(val.MapIndex(key))
 		}
 	case reflect.Chan:
-		//		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
 		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
 			walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walk(res.Interface(), fn)
 		}
 	}
 }
